49e18ebcea3675af885bd1c14c280661f2afbc51
devwithu <devwithu@naver.com>
2020-02-26 07:01:58 +0900

    2/7

--- Assets/Activator.cs
+++ Assets/Activator.cs
@@ -8,17 +8,28 @@ public class Activator : MonoBehaviour
     public KeyCode key;
     private bool active = false;
     private GameObject note;
+    private SpriteRenderer sr;
+    private Color old ;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    private void Start()
+    {
+        old = sr.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(key) )
+            StartCoroutine(Pressed());
+            
         if (Input.GetKeyDown(key) && active)
         {
             Destroy(note );
@@ -37,5 +48,13 @@ public class Activator : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         active = false;
+    }
+
+    IEnumerator Pressed()
+    {
+        
+        sr.color = new Color(0,0,0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
     }
 }
